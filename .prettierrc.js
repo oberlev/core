046a988d67e56ea1494ff654029c29a0c9e6da65
Daniil Oberlev <denioberlev@gmail.com>
2026-04-23 22:55:22 +0300

    feat(incapsulation): add prettier package

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,3 @@
-/** @type {import("prettier").Config} */
-export default {
-  printWidth: 100,
-  singleQuote: true,
-  jsxSingleQuote: true,
-  trailingComma: 'none',
-  semi: true,
-  tabWidth: 2,
-  useTabs: false,
-  endOfLine: 'lf',
-  arrowParens: 'always'
-};
+import { prettier } from '@oberlev/prettier';
+
+export default prettier;
